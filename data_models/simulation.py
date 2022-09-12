202ca45605e58aa64496e32fe2b86edac2ab95a7
ebauschatz <eaenglish113@gmail.com>
2022-09-12 13:45:15 -0500

    feat: adding logging for customer order transactions

--- data_models/simulation.py
+++ data_models/simulation.py
@@ -1,9 +1,13 @@
 from data_models.franchise import Franchise
+from data_models.logger import transaction_log
 
 class Simulation:
     def __init__(self):
         pass
 
     def run_simulation(self):
+        transaction_log.initialize_daily_log()
+        
         franchise_one = Franchise(1)
+        franchise_one.place_order()
         franchise_one.place_order()
